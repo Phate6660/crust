dcee2be551958ce8feb8bc5c9d8d12454c0f1da2
ValleyKnight <ValleyKnight@protonmail.com>
2021-08-26 10:36:13 -0400

    combine ls functions into one

--- src/ls.rs
+++ src/ls.rs
@@ -14,50 +14,20 @@ pub fn ls(input: &str) {
     }
     for file in path {
         let raw_entry = file.unwrap().path();
-        let still_raw_entry = raw_entry.to_str().unwrap().replace("./", "");
-        let paths = still_raw_entry.split('\n');
-        for line in paths {
-            let parts = line.split('/');
-            let mut n = 0;
-            let parts_count = line.split('/').count();
-            for part in parts {
-                if part.starts_with('.') || n == parts_count - 1 {
-                    t.fg(term::color::WHITE).unwrap();
-                } else {
-                    t.fg(term::color::GREEN).unwrap();
-                }
-                write!(t, "{}", part).unwrap();
-                t.reset().unwrap();
-                n += 1;
-                if n == parts_count {
-                    break;
-                } else {
-                    write!(t, "/").unwrap();
-                }
-            }
-            println!();
-        }
-    } 
-}
-
-#[cfg(target_os = "windows")]
-pub fn ls(input: &str) {
-    let mut t = term::stdout().unwrap();
-
-    let path;
-    if std::path::Path::new(input).exists() {
-        path = std::fs::read_dir(input).unwrap()
-    } else {
-        println!("ERROR: '{}' is not a valid file or directory.", input);
-        return
-    }
-    for file in path {
-        let raw_entry = file.unwrap().path();
+        #[cfg(target_os = "linux")]
+        let still_raw_entry = raw_entry.to_str().unwrap().replace("./", ""); 
+        #[cfg(target_os = "windows")]
         let still_raw_entry = raw_entry.to_str().unwrap().replace(".\\", "");
         let paths = still_raw_entry.split('\n');
         for line in paths {
+            #[cfg(target_os = "linux")]
+            let parts = line.split('/');
+            #[cfg(target_os = "windows")]
             let parts = line.split('\\');
             let mut n = 0;
+            #[cfg(target_os = "linux")]
+            let parts_count = line.split('/').count();
+            #[cfg(target_os = "windows")]
             let parts_count = line.split('\\').count();
             for part in parts {
                 if part.starts_with('.') || n == parts_count - 1 {
@@ -71,6 +41,9 @@ pub fn ls(input: &str) {
                 if n == parts_count {
                     break;
                 } else {
+                    #[cfg(target_os = "linux")]
+                    write!(t, "/").unwrap();
+                    #[cfg(target_os = "windows")]
                     write!(t, "\\").unwrap();
                 }
             }
