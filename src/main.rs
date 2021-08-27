af1645e2ba7ca8916191d70b654a95005fe8297a
ValleyKnight <ValleyKnight@protonmail.com>
2021-08-27 11:47:11 -0400

    some slight refactoring and much better piping support

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,65 @@ fn parse_input(op: &str) -> String {
     }
 }
 
+fn piped(input: &str) {
+    let input = input.split('|').collect::<Vec<&str>>();
+    let mut cmd1 = input[0].split(' ').collect::<Vec<&str>>();
+    let mut cmd2 = input[1].split(' ').collect::<Vec<&str>>();
+    cmd1.pop();
+    cmd2.remove(0);
+    let child1 = Command::new(cmd1[0])
+        .args(&cmd1[1..])
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .output()
+        .or(Err(()));
+    if child1.is_err() {
+        println!("Sorrry, '{}' was not found!", input[0]);
+    } else {
+        let child2 = match Command::new(cmd2[0])
+            .args(&cmd2[1..])
+            .stdin(std::process::Stdio::piped())
+            .stdout(std::process::Stdio::piped())
+            .spawn() {
+                Err(why) => panic!("couldn't spwn cmd2: {}", why),
+                Ok(child2) => child2,
+            };
+        match child2.stdin.unwrap().write_all(String::from_utf8_lossy(&child1.unwrap().stdout).trim().as_bytes()) {
+            Err(why) => println!("ERROR: couldn't write cmd2 stdin because of {}", why),
+            Ok(_) => (),
+        }
+        let mut output = String::new();
+        match child2.stdout.unwrap().read_to_string(&mut output) {
+            Err(why) => println!("could not read cmd2 stdout: {}", why),
+            Ok(_) => println!("{}", output.trim()),
+        }
+    }
+}
+
+fn cmd(input: &str, args: bool) {
+    if args {
+        let input = input.split(' ').collect::<Vec<&str>>();
+        let child = Command::new(&input[0])
+            .args(&input[1..])
+            .spawn()
+            .or(Err(()));
+        if child.is_err() {
+            println!("Sorry, '{}' was not found!", input[0]);
+        } else {
+            child.unwrap().wait().unwrap();
+        }
+    } else {
+        let child = Command::new(&input)
+            .spawn()
+            .or(Err(()));
+        if child.is_err() {
+            println!("Sorry, '{}' was not found!", input);
+        } else {
+            child.unwrap().wait().unwrap();
+        }
+    }
+}
+
 fn run_command(input: String) {
     if input.starts_with("calc") {
         calc_run(&input);
@@ -83,50 +142,12 @@ fn run_command(input: String) {
         }
     } else if input == "pwd" {
         println!("{}", std::env::current_dir().unwrap().display());
+    } else if input.contains('|') {
+        piped(&input);
     } else if input.contains(' ') {
-        if input.contains('|') {
-            let input = input.split('|').collect::<Vec<&str>>();
-            let mut cmd1 = input[0].split(' ').collect::<Vec<&str>>();
-            let mut cmd2 = input[1].split(' ').collect::<Vec<&str>>();
-            cmd1.pop();
-            cmd2.remove(0);
-            let child1 = Command::new(cmd1[0])
-                .args(&cmd1[1..])
-                .stdin(std::process::Stdio::piped())
-                .stdout(std::process::Stdio::piped())
-                .output()
-                .or(Err(()));
-            if child1.is_err() {
-                println!("Sorrry, '{}' was not found!", input[0]);
-            } else {
-                let child2 = match Command::new(cmd2[0])
-                    .args(&cmd2[1..])
-                    .stdin(std::process::Stdio::piped())
-                    .stdout(std::process::Stdio::piped())
-                    .spawn() {
-                        Err(why) => panic!("couldn't spwn cmd2: {}", why),
-                        Ok(child2) => child2,
-                };
-                match child2.stdin.unwrap().write_all(String::from_utf8_lossy(&child1.unwrap().stdout).trim().as_bytes()) {
-                    Err(why) => println!("ERROR: couldn't write cmd2 stdin because of {}", why),
-                    Ok(_) => (),
-                }
-                let mut output = String::new();
-                match child2.stdout.unwrap().read_to_string(&mut output) {
-                    Err(why) => println!("could not read cmd2 stdout: {}", why),
-                    Ok(_) => println!("{}", output.trim()),
-                }
-            }
-        }
+        cmd(&input, true);
     } else {
-        let child = Command::new(&input)
-            .spawn()
-            .or(Err(()));
-        if child.is_err() {
-            println!("Sorry, '{}' was not found!", input);
-        } else {
-            child.unwrap().wait().unwrap();
-        }
+        cmd(&input, false);
     }
 }
 
