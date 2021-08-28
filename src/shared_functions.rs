cd9c2630809ba66349487cdf28adb919ac5c1fe0
ValleyKnight <ValleyKnight@protonmail.com>
2021-08-27 22:00:54 -0400

    some shared_functions.rs updates

--- src/shared_functions.rs
+++ src/shared_functions.rs
@@ -1,7 +1,7 @@
 #[cfg(not(feature = "readline"))]
 use std::io::Write;
 
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::io::Read;
 
 pub fn cd_helper(dir: &str) {
@@ -12,7 +12,7 @@ pub fn cd_helper(dir: &str) {
 
 pub fn cmd(input: &str, args: bool) {
     if args {
-        let input = input.split(' ').collect::<Vec<&str>>();
+        let input: Vec<&str> = input.split(' ').collect();
         let child = Command::new(&input[0])
             .args(&input[1..])
             .spawn()
@@ -60,7 +60,7 @@ pub fn non_interactive() {
 
 pub fn parse_input(op: &str) -> String {
     if op == "interactive" {
-        let mut input = std::string::String::new();
+        let mut input = String::new();
         std::io::stdin()
             .read_line(&mut input)
             .expect("failed to read user input");
@@ -74,15 +74,15 @@ pub fn parse_input(op: &str) -> String {
 }
 
 pub fn piped_cmd(input: &str) {
-    let input = input.split('|').collect::<Vec<&str>>();
-    let mut cmd1 = input[0].split(' ').collect::<Vec<&str>>();
-    let mut cmd2 = input[1].split(' ').collect::<Vec<&str>>();
+    let input: Vec<&str> = input.split('|').collect();
+    let mut cmd1: Vec<&str> = input[0].split(' ').collect();
+    let mut cmd2: Vec<&str> = input[1].split(' ').collect();
     cmd1.pop();
     cmd2.remove(0);
     let child1 = Command::new(cmd1[0])
         .args(&cmd1[1..])
-        .stdin(std::process::Stdio::piped())
-        .stdout(std::process::Stdio::piped())
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
         .output()
         .or(Err(()));
     if child1.is_err() {
@@ -90,19 +90,21 @@ pub fn piped_cmd(input: &str) {
     } else {
         let child2 = match Command::new(cmd2[0])
             .args(&cmd2[1..])
-            .stdin(std::process::Stdio::piped())
-            .stdout(std::process::Stdio::piped())
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
             .spawn() {
-                Err(why) => panic!("couldn't spwn cmd2: {}", why),
+                Err(why) => panic!("ERROR: couldn't spwn cmd2: {}", why),
                 Ok(child2) => child2,
             };
-        match child2.stdin.unwrap().write_all(String::from_utf8_lossy(&child1.unwrap().stdout).trim().as_bytes()) {
-            Err(why) => println!("ERROR: couldn't write cmd2 stdin because of {}", why),
+        match child2.stdin.unwrap().write_all(
+            String::from_utf8_lossy(&child1.unwrap().stdout).trim().as_bytes()
+        ) {
+            Err(why) => println!("ERROR: couldn't write to cmd2's stdin because of {}", why),
             Ok(_) => (),
         }
         let mut output = String::new();
         match child2.stdout.unwrap().read_to_string(&mut output) {
-            Err(why) => println!("could not read cmd2 stdout: {}", why),
+            Err(why) => println!("ERROR: could not read cmd2 stdout: {}", why),
             Ok(_) => println!("{}", output.trim()),
         }
     }
@@ -112,8 +114,8 @@ pub fn piped_text(input: &str, args: bool, cmd: Vec<&str>) {
     if args {
         let child = match Command::new(cmd[0])
             .args(&cmd[1..])
-            .stdin(std::process::Stdio::piped())
-            .stdout(std::process::Stdio::piped())
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
             .spawn() {
                 Err(why) => panic!("couldn't spwn cmd: {}", why),
                 Ok(child) => child,
@@ -129,8 +131,8 @@ pub fn piped_text(input: &str, args: bool, cmd: Vec<&str>) {
         }
     } else {
         let child = match Command::new(cmd[0])
-            .stdin(std::process::Stdio::piped())
-            .stdout(std::process::Stdio::piped())
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
             .spawn() {
                 Err(why) => panic!("couldn't spwn cmd: {}", why),
                 Ok(child) => child,
