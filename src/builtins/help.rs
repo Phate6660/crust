fd9ada58935077ba1beee8785caa649619cc1c4f
ValleyKnight <ValleyKnight@protonmail.com>
2021-12-14 11:11:29 -0500

    fix some weird formatting

--- src/builtins/help.rs
+++ src/builtins/help.rs
@@ -1,8 +1,7 @@
 /// Prints a list of builtin commands.
 pub fn help(args: &[String]) {
     if args.is_empty() {
-        println!(
-            "\
+        println!("\
             crust [https://github.com/Phate6660/crust]\n\
             builtins:\n\
             ---------\n\
@@ -23,15 +22,9 @@ pub fn help(args: &[String]) {
     }
     match args[0].as_str() {
         "calc" => println!("Supports +, -, /, x for two numbers.\nE.g. 1+1, 4/2, 2x4, 2-1"),
-        "cat" => {
-            println!(
-                "\
-                Support printing file output, and additionally showing line numbers with `-n`."
-            );
-        }
+        "cat" => println!("Support printing file output, and additionally showing line numbers with `-n`."),
         "cd" => {
-            println!(
-                "\
+            println!("\
                 Takes a absolute or relative path and changes directory to it.\n\
                 `cd -` will take you to your previous dir."
             );
@@ -42,8 +35,7 @@ pub fn help(args: &[String]) {
         "ls" => println!("Lists the content of a directory."),
         "pwd" => println!("Prints the working directory."),
         "prompt" => {
-            println!(
-                "\
+            println!("\
                 Can be set to a static string, by just setting the string in the PROMPT env variable,\n\
                 or can be set to a dynamic prompt, by including a command to be executed, by delimiting it with %(),\
                 in the prompt string.\n\
@@ -52,8 +44,7 @@ pub fn help(args: &[String]) {
             );
         }
         _ => {
-            println!(
-                "\
+            println!("\
                 cRUSTy [https://github.com/Phate6660/crust]\n\
                 builtins:\n\
                 ---------\n\
